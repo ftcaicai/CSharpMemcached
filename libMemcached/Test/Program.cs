2de0ee134fe21c322b9c42d83d0f702a651bffe9
FtLover <ftcaicai@gmail.com>
2013-09-09 14:46:21 +0800

    Add Lib

---
+++ libMemcached/Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memcached.ClientLibrary;
+using System.Collections;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList services = new ArrayList();
+            services.Add("127.0.0.1:11211");
+            SockIOPool sock = SockIOPool.GetInstance();
+            sock.SetServers(services);
+            sock.Initialize();
+
+            MemcachedClient client = new MemcachedClient();
+            client.Set("F", "xxx");
+
+            Console.WriteLine("F:" + client.Get("F"));
+
+            Console.ReadKey();
+        }
+    }
+}
